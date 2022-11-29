d27d2528bcd3083147ae57a3326f7f4920a727c4
Iamprem-1998 <117341305+Iamprem-1998@users.noreply.github.com>
2022-11-29 11:54:25 +0530

    Add files via upload

---
+++ _2_question2.py
@@ -0,0 +1,4 @@
+#Write a Python program to reverse a string.
+def reverse(str1):
+    print(str1[::-1])
+reverse("1234wert")
---
+++ __1_question1.py
@@ -0,0 +1,5 @@
+#Write a Python function to sum all the numbers in a list.
+def add(no1,no2,no3,no4):
+    print(no1+no2+no3+no4)
+add(2,5,6,8)
+ 
